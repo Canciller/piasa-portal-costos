5380a031ea82376db2fac83f7954bad748e1599f
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-08-27 23:25:35 -0500

    Added create initial user

--- server/src/util/connectWithRetry.js
+++ server/src/util/connectWithRetry.js
@@ -1,10 +1,33 @@
 import sql from 'mssql';
 import config from '../config/db';
 import debug from '../util/log/debug';
+import User from '../models/user.model';
+import hashPassword from './hashPassword';
 
 var retryWait = process.env.DB_RETRY_WAIT_SECONDS || 5;
 var maxRetries = process.env.DB_MAX_RETRIES;
 var currentTry = 1;
+
+require('dotenv').config();
+
+async function createAdminUser() {
+  var username = process.env.ADMIN_USERNAME || 'admin',
+    password = process.env.ADMIN_PASSWORD || 'admin',
+    email = process.env.ADMIN_EMAIL || 'admin@admin.com',
+    name = process.env.ADMIN_NAME || 'Admin';
+
+  try {
+    var all = await User.getAll();
+    if(all.length === 0) {
+      var password = hashPassword(password);
+      var user = new User(username, name, email, password, true, 'A');
+      await User.create(user);
+      debug(`Created initial user '${username}'`);
+    }
+  } catch(error) {
+    console.error(`Error creating initial user '${username}', already created or failed to get all users.`);
+  }
+}
 
 export default function connectWithRetry() {
   sql.connect(config, (error) => {
@@ -22,6 +45,7 @@ export default function connectWithRetry() {
       setTimeout(connectWithRetry, retryWait * 1000);
     } else {
       debug(`Connected to Database at ${config.server}`);
+      createAdminUser();
     }
   });
 }
