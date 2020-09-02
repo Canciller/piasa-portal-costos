ef656829ec1fce7de0f14dcd92cf87b1ef1c7118
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-09-01 20:24:38 -0500

    Fixed typo and increased request timeout

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,9 +3,14 @@ export default {
   password: process.env.DB_PASS,
   server: process.env.DB_HOST,
   database: process.env.DB_NAME,
+  connectionTimeout: 300000,
+  requestTimeout: 300000,
+  pool: {
+      idleTimeoutMillis: 300000,
+  },
   options: {
     enableArithAbort: true,
     encrypt: false,
   },
-  debug: process.env.NODE_ENV === 'developmet',
+  debug: process.env.NODE_ENV === 'development',
 };
